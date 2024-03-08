5006d761d1cc0c38697127d9868b74539317c2a4
Abdullah <abdulhaseeb9274@gmail.com>
2024-03-08 21:59:48 +0500

    about page picture changes

--- src/components/AboutPage/AboutSection.jsx
+++ src/components/AboutPage/AboutSection.jsx
@@ -12,6 +12,7 @@ export default function AboutSection() {
         alt="Abdullah Profile Picture"
         width={800}
         height={800}
+        loading = 'eager'
         priority
       />
       {/* about content  */}
